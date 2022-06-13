af9bad8c556df0bf45b9cb6915b781d85eab6f04
Ugur Unlu <vally10@live.com>
2022-06-13 14:14:55 +0200

    Initial Commit

---
+++ Navigaton SwiftUI4 Sample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Navigaton SwiftUI4 Sample
+//
+//  Created by Ugur Unlu on 13/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigaton SwiftUI4 Sample/Navigaton_SwiftUI4_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigaton_SwiftUI4_SampleApp.swift
+//  Navigaton SwiftUI4 Sample
+//
+//  Created by Ugur Unlu on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Navigaton_SwiftUI4_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
